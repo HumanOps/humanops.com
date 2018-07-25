1bb307dbea89cbaac35d2a2ea7a583598b1ddb97
Harry Wincup <harry@harrywincup.co.uk>
2018-07-25 17:31:34 +0100

    Add the <meta /> info from the old site

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -21,6 +21,8 @@ export default class HumanOpsDocument extends Document {
         return (
             <html lang='en'>
                 <Head>
+                    <meta charset="utf-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3.0, minimum-scale=1" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
@@ -31,27 +33,48 @@ export default class HumanOpsDocument extends Document {
                     <meta name="msapplication-TileColor" content="#00aba9" />
                     <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
                     <meta name="theme-color" content="#ffffff" />
+                    <link rel="canonical" href="http://www.humanops.com/" />
+
+                    <meta name="description" content="HumanOps is a set of principles which focus on the human aspects of running infrastructure. The health of your infrastructure is not just about hardware, software, automations and uptime - it also includes the health and wellbeing of your team." />
+                    <meta property="og:title" content="HumanOps" />
+                    <meta property="og:description" content="HumanOps is a set of principles which focus on the human aspects of running infrastructure. The health of your infrastructure is not just about hardware, software, automations and uptime - it also includes the health and wellbeing of your team." />
+                    <meta property="og:url" content="http://www.humanops.com/" />
+                    <meta property="og:site_name" content="HumanOps" />
+                    <meta property="og:image" content="/assets/images/humanops-banner.png" />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:site" content="@humanops" />
+                    <meta name="twitter:creator" content="@humanops" />
+                    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `
+                        {
+                            "@context": "http://schema.org",
+                                "@type": "WebSite",
+                                "name": "HumanOps",
+                                "headline": "HumanOps",
+                                "description": "HumanOps is a set of principles which focus on the human aspects of running infrastructure. The health of your infrastructure is not just about hardware, software, automations and uptime - it also includes the health and wellbeing of your team.",
+                                "url": "http://www.humanops.com/"
+                        }
+                    `}} />
 
                     {this.props.styleTags}
 
                     <style dangerouslySetInnerHTML={{__html: `
-                        * {
-                            box-sizing: border-box;
-                            margin: 0;
-                            padding: 0;
-                        }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-                        html {
-                            position: relative;
-                            font-size: ${ Typography.baseFontSize }px;
-                            background: ${ Color.brand('primary') };
-                        }
-                        
-                        ::selection {
-                            color: ${ Color.lightest };
-                            background: ${ Color.brand('cta') };
-                        }
-                    ` }} />
+    html {
+        position: relative;
+    font-size: ${ Typography.baseFontSize }px;
+    background: ${ Color.brand('primary') };
+    }
+
+    ::selection {
+    color: ${ Color.lightest };
+    background: ${ Color.brand('cta') };
+    }
+` }} />
                 </Head>
                 <body>
                     <Main />
@@ -59,14 +82,14 @@ export default class HumanOpsDocument extends Document {
 
                     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js"></script>
                     <script dangerouslySetInnerHTML={{__html: `
-                        WebFont.load({
-                            google: {
-                                families: ['Nunito:300,400,700']
-                            }
-                        })
-                    ` }} />
+    WebFont.load({
+        google: {
+        families: ['Nunito:300,400,700']
+    }
+})
+` }} />
                 </body>
             </html>
-        )
-    }
+    )
+}
 }
